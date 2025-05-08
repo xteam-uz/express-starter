8a68349332a323e6933473df1db51da92c038b7c
mirrrrrrrrrrrr <mirrrrrr@yahoo.com>
2025-05-08 12:40:26 +0500

    Step 02: setup express

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { json, type Application } from "express";
 import restaurantsRouter from "@routes/restaurants.js";
 import cusinisRouter from "@routes/cuisines.js";
+import { errorHandler } from "@middlewares/errorHandler.js";
 
 const App = (app: Application): void => {
   app.use(json());
@@ -8,6 +9,9 @@ const App = (app: Application): void => {
   // routes
   app.use("/api/restaurants", restaurantsRouter);
   app.use("/api/cusines", cusinisRouter);
+
+  // middlewares
+  app.use(errorHandler);
 };
 
 export default App;
